a907c92551d1e8724ebd7fa316eab21e5e8ba177
yoclo <3286108868@qq.com>
2026-08-24 22:31:04 +0800

    chore(build): update AndroidX Hilt and compile SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.aeibi.design"
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.aeibi.design"
         minSdk = 26
